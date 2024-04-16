87715c8a7e63760d056571839fc40aaf690374f8
Mkhotamirais <mkhotamirais@gmail.com>
2024-04-17 00:00:33 +0700

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,19 @@
 const Footer = () => {
   return (
-    <footer className="border-t h-16">
-      <div className="flex items-center justify-center h-full">Footer</div>
+    <footer className="h-16 border-t">
+      <div className="flex h-full items-center justify-center">
+        <div className="order-2 sm:order-1">
+          Copyright <sup>&copy;</sup> {new Date().getFullYear()}{" "}
+          <a
+            href="https://mkhotamirais.github.io/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-cyan-600 hover:underline"
+          >
+            mkhotami
+          </a>
+        </div>{" "}
+      </div>
     </footer>
   );
 };
